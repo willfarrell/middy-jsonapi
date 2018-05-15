87bccfbbac9d3f2fd9c631968e36a49e6aa5f080
will Farrell <will.farrell@gmail.com>
2018-05-15 08:31:39 -0600

    feat: init commit

--- README.md
+++ README.md
@@ -1,2 +1,115 @@
 # middy-jsonapi
-JSONAPI middleware
+JSONAPI middleware for middy
+
+## Why
+- format lambda response to meet the [jsonapi](http://jsonapi.org) standard
+
+## Getting Started
+```bash
+npm i middy middy-jsonapi
+```
+
+### Requirements
+See below for full example.
+
+#### Accept Header
+The `Accept` is required to be `application/vnd.api+json`
+```
+.use(
+      httpContentNegotiation({
+        availableMediaTypes: ['application/vnd.api+json']
+      })
+    )
+```
+
+#### Validation
+This middleware expects `400 Bad Request` to be returned from the `validator` middleware provided from `middy/middleware` or equivalent [`ajv`](https://github.com/epoberezkin/ajv) error format.
+
+## Features
+### Request
+- Parses `include`, `fields`, `sort`, `page` query parameters
+- See `middy-jsonapi-filter-*` for `filter` parsing [TODO]
+
+### Response
+- Format errors to meet standard
+- Supports `page-based` and `offset-based` pagination auto generate `links` from `meta.total` & `meta.count`
+
+## Deployment
+
+```javascript
+const middy = require('middy')
+const {
+  doNotWaitForEmptyEventLoop,
+  urlEncodeBodyParser,
+  jsonBodyParser,
+  cors,
+  httpEventNormalizer,
+  httpHeaderNormalizer,
+  httpContentNegotiation,
+  validator
+} = require('middy/middlewares')
+const jsonapi = require('middy-jsonapi')
+const authorization = require('../middleware/authorization')
+
+const ajvOptions = {
+  v5: true,
+  format: 'full',
+  coerceTypes: 'array',
+  allErrors: true,
+  useDefaults: true,
+  $data: true
+}
+
+const meta = require('../../package.json')
+const response = {
+  jsonapi: { version:'1.0' },
+  meta: {
+    version: `v${meta.version}`,
+    copyright: meta.copyright,
+    authors: meta.authors,
+    now: new Date().toISOString()
+  }
+}
+
+module.exports = (app, { inputSchema, outputSchema }) =>
+  middy(app)
+    .use(doNotWaitForEmptyEventLoop())
+    .use(httpEventNormalizer())
+    .use(httpHeaderNormalizer())
+    .use(urlEncodeBodyParser())
+    .use(jsonBodyParser())
+    .use(cors())
+    .use(jsonapi({ response })) // Replaces: httpErrorHandler
+    .use(
+      httpContentNegotiation({
+        availableLanguages: ['en-CA', 'fr-CA'],
+        availableMediaTypes: ['application/vnd.api+json']
+      })
+    )
+    //.use(authorization())
+    .use(validator({ inputSchema, outputSchema, ajvOptions }))
+```
+
+### Response Deserialization
+We recommend [`kitsu-core`](https://github.com/wopian/kitsu) for deserializing 
+
+```javascript
+const { deserialise } = require('kitsu-core/node')
+
+const jsonapiDeserialise = body => {
+  deserialise(body)
+  return body
+}
+```
+
+## Built With
+- [middy](https://github.com/middyjs/middy)
+
+## Authors
+- [willfarrell](https://github.com/willfarrell/)
+
+## License
+This project is licensed under the MIT License - see the LICENSE file for details
+
+## TODO
+- Add in jsonapi ~= for httpPartialResponse
---
+++ index.js
@@ -0,0 +1,313 @@
+const { URL, URLSearchParams } = require('url')
+const createError = require('http-errors')
+
+const JsonApiQueryParserClass = require('jsonapi-query-parser')
+const jsonapiQueryParser = new JsonApiQueryParserClass()
+
+const defaults = {
+  response: {
+    jsonapi: { version: '1.0' }
+  },
+  pagination: 'offset-based',
+  logger: console.error
+}
+
+const querySerialize = function (obj) {
+  const str = []
+  Object.keys(obj).forEach(p => {
+    str.push(encodeURIComponent(p) + '=' + encodeURIComponent(obj[p]))
+  })
+  return str.join('&')
+}
+
+// Docs
+// Includes middy/middleware/httpErrorHandler handling
+// Doesn't included filter parsing
+
+const request = (handler, next) => {
+  // parse
+  if (handler.event.queryStringParameters) {
+    handler.event.queryStringParametersRaw = handler.event.queryStringParameters // Used for `links` in response
+    const { include, fields, sort, page } = jsonapiQueryParser.parseRequest(
+      '?' + querySerialize(handler.event.queryStringParameters)
+    ).queryData
+    const params = { include, fields, sort, page }
+
+    const disallowed = ['include', 'fields', 'sort', 'page']
+    Object.keys(handler.event.queryStringParameters)
+      .filter(key => !disallowed.includes(key.split('[')[0]))
+      .forEach(key => {
+        params[key] = handler.event.queryStringParameters[key]
+      })
+
+    handler.event.queryStringParameters = params
+    console.log(params)
+  }
+
+  return next()
+}
+
+const httpError = handler => {
+  if (
+    !(handler.error instanceof createError.HttpError) ||
+    Array.isArray(handler.error.details)
+  ) {
+    return {}
+  }
+
+  handler.response.statusCode = handler.error.statusCode
+  handler.response.body = Object.assign({}, handler.response.body, {
+    errors: [
+      {
+        status: handler.error.statusCode.toString(),
+        title: handler.error.name,
+        detail: handler.error.message
+      }
+    ]
+  })
+  return handler.response
+}
+
+const jsonschemaErrors = handler => {
+  if (
+    !(handler.error instanceof createError.BadRequest) ||
+    !Array.isArray(handler.error.details)
+  ) {
+    return {}
+  }
+  handler.response.statusCode = handler.error.statusCode
+  handler.response.body = Object.assign({}, handler.response.body, {
+    errors: handler.error.details.map(error => ({
+      detail: error.message,
+      source: {
+        pointer: error.schemaPath.substr(1),
+        parameter: error.dataPath.substr(1)
+      },
+      meta: error.params
+    }))
+  })
+  return handler.response
+}
+
+const unhandledError = handler => {
+  if (!(handler.error instanceof Error)) return {}
+  console.error(handler.context.awsRequestId, handler.error)
+
+  handler.response.statusCode = 500
+  handler.response.body = Object.assign({}, handler.response.body, {
+    errors: [
+      {
+        status: '500',
+        title: 'Internal Server Error',
+        detail: 'Please contact admin and provide `X-Request-ID` value.'
+      }
+    ]
+  })
+}
+
+const linkUrl = handler => {
+  const params = new URLSearchParams(handler.event.queryStringParametersRaw)
+  const url = new URL(
+    `${handler.event.path}?${params.toString()}`,
+    `https://${handler.event.headers['Host']}` // Note: https assumed
+  )
+  const urlPrefix = `${
+    url.host.substr(0, 9) !== 'localhost' // workaround for serverless-offline
+      ? url.protocol
+      : 'http'
+  }//${url.host}${url.pathname}`
+
+  return { urlPrefix, url }
+}
+
+const pageBasedPagination = handler => {
+  // page[number], page[size]
+  const body = handler.response.body
+  if (
+    !body.meta ||
+    !body.meta.item ||
+    typeof body.meta.item.count !== 'number' ||
+    typeof body.meta.item.total !== 'number'
+  ) {
+    return body
+  }
+
+  const { count, total } = body.meta.item
+  if (count > total) {
+    console.error(handler.context.awsRequestId, 'Row count > total rows.')
+    return body
+  }
+
+  const { urlPrefix, url } = linkUrl(handler)
+
+  const limit =
+    handler.event.queryStringParameters &&
+    Number.parseInt(handler.event.queryStringParameters.page.size, 10) >= 1
+      ? Number.parseInt(handler.event.queryStringParameters.page.size, 10)
+      : total
+
+  const pages = Math.ceil(total / limit)
+  const page = Number.parseInt(
+    handler.event.queryStringParameters.page.number,
+    10
+  )
+
+  if (pages === 1) return body
+
+  body.meta.page = {}
+  body.meta.page.first = 1
+  body.meta.page.prev = page > 1 ? page + 1 : null
+  body.meta.page.self = page
+  body.meta.page.next = page < pages ? page + 1 : null
+  body.meta.page.last = pages
+
+  body.links = {}
+  body.links.self = urlPrefix + url.search
+
+  url.searchParams.set('page[number]', body.meta.page.first)
+  body.links.first = urlPrefix + url.search
+
+  url.searchParams.set('page[number]', body.meta.page.last)
+  body.links.last = urlPrefix + url.search
+
+  if (page > 1) {
+    url.searchParams.set('page[number]', body.meta.page.prev)
+    body.links.prev = urlPrefix + url.search
+  }
+
+  if (page < pages) {
+    url.searchParams.set('page[number]', body.meta.page.next)
+    body.links.next = urlPrefix + url.search
+  }
+
+  return handler.response.body
+}
+
+const offsetBasedPagination = handler => {
+  // page[limit], page[offset]
+  const body = handler.response.body
+  if (
+    !body.meta ||
+    !body.meta.item ||
+    typeof body.meta.item.count !== 'number' ||
+    typeof body.meta.item.total !== 'number'
+  ) {
+    return body
+  }
+
+  const { count, total } = body.meta.item
+  if (count > total) {
+    console.error(handler.context.awsRequestId, 'Row count > total rows.')
+    return body
+  }
+
+  const { urlPrefix, url } = linkUrl(handler)
+
+  const limit =
+    handler.event.queryStringParameters &&
+    Number.parseInt(handler.event.queryStringParameters.page.limit, 10) >= 1
+      ? Number.parseInt(handler.event.queryStringParameters.page.limit, 10)
+      : 1 // 1 to prevent division by zero
+  const offset =
+    handler.event.queryStringParameters &&
+    Number.parseInt(handler.event.queryStringParameters.page.offset, 10) >= 0
+      ? Number.parseInt(handler.event.queryStringParameters.page.offset, 10)
+      : 0
+
+  const pages = Math.ceil(total / limit)
+  const page = Math.ceil(offset / limit) + 1
+
+  body.meta.page = {}
+  body.meta.page.first = 1
+  body.meta.page.prev = page > 1 ? page + 1 : null
+  body.meta.page.self = page
+  body.meta.page.next = page < pages ? page + 1 : null
+  body.meta.page.last = pages
+
+  body.links = {}
+  body.links.self = urlPrefix + url.search
+
+  url.searchParams.set('page[offset]', 0)
+  body.links.first = urlPrefix + url.search
+
+  url.searchParams.set('page[offset]', limit * (pages - 1))
+  body.links.last = urlPrefix + url.search
+
+  if (page > 1) {
+    url.searchParams.set('page[offset]', limit * (page - 2))
+    body.links.prev = urlPrefix + url.search
+  }
+
+  if (page < pages) {
+    url.searchParams.set('page[offset]', limit * page)
+    body.links.next = urlPrefix + url.search
+  }
+
+  return body
+}
+
+const cursorBasedPagination = handler => {
+  // page[cursor]
+  // TODO if requested
+  return handler.response.body
+}
+
+const pagination = {
+  'page-based': pageBasedPagination,
+  'offset-based': offsetBasedPagination,
+  'cursor-based': cursorBasedPagination
+}
+
+const response = (opts, handler, next) => {
+  const options = Object.assign({}, defaults, opts)
+  const response = options.response
+
+  handler.response = handler.response || {}
+  handler.response.headers = Object.assign(
+    {},
+    {
+      'Content-Type': 'application/vnd.api+json',
+      'Content-Language': handler.event.preferredLanguage // TODO refactor out
+    },
+    handler.response.headers
+  )
+
+  // add in meta
+  if (response.meta || handler.response.body.meta) {
+    handler.response.body.meta = Object.assign(
+      {},
+      response.meta,
+      handler.response.body.meta || {}
+    )
+  } // Ensure meta is preserved, assign isn't deep
+  handler.response.body = Object.assign({}, response, handler.response.body)
+  // catch any errors
+  if (handler.error) {
+    if (typeof options.logger === 'function') {
+      options.logger(handler.context.awsRequestId, handler.error)
+    }
+    handler.response = Object.assign(
+      {},
+      handler.response,
+      httpError(handler),
+      jsonschemaErrors(handler),
+      unhandledError(handler)
+    )
+
+    return next()
+  }
+
+  // catch any stringified bodies
+  if (typeof handler.response.body === 'string') {
+    handler.response.body = JSON.parse(handler.response.body)
+  }
+
+  handler.response.body = pagination[options.pagination](handler)
+  return next()
+}
+
+module.exports = opts => ({
+  before: request,
+  after: response.bind(null, opts),
+  onError: response.bind(null, opts)
+})
